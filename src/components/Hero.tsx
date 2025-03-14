4d064bc9954c1e4a6b780d90520fd30df68f1a86
abhises <abhisespoudyal@gmail.com>
2025-03-14 09:29:17 +0545

    adding name of own

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,8 +9,8 @@ const Hero = () => {
     {
       quote:
         "The attention to detail and innovative features have completely transformed our workflow. This is exactly what we've been looking for.",
-      name: "Sarah Chen",
-      designation: "Product Manager at TechFlow",
+      name: "Abhises POudyal",
+      designation: "Fullstack developer",
       src: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=3560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
