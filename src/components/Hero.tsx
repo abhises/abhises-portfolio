9e940b8987f838d93cac8cad9da0d701b6374e1c
abhises <abhisespoudyal@gmail.com>
2025-03-14 10:34:40 +0545

    added profile image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,16 +2,16 @@
 
 import { TextGenerateEffect } from "./ui/text-generate-effect";
 import { AnimatedTestimonials } from "./ui/animated-testimonials";
-const words = `Hello! My name is Abhises Poduyal. I am fullstack developer
+const words = `Hello! My name is Abhises Poudyal. I am fullstack developer
 `;
 const Hero = () => {
   const testimonials = [
     {
       quote:
         "The attention to detail and innovative features have completely transformed our workflow. This is exactly what we've been looking for.",
-      name: "Abhises POudyal",
+      name: "Abhises Poudyal",
       designation: "Fullstack developer",
-      src: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=3560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      src: "/assets/profileimage.jpg",
     },
     {
       quote:
